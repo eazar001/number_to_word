caf7600911f88fb5611e5d69542fc925f7bd85e9
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-10-31 22:06:27 -0700

    Initial commit.

---
+++ groups.pl
@@ -0,0 +1,112 @@
+
+:- module(groups, [ group//1
+		   ,unit/2
+		   ,ten/2
+		   ,beyond/2 ]).
+
+
+:- use_module(library(clpfd)).
+
+
+term_expansion(beyond(Goal, Word), Goal0) :-
+  Num is Goal,
+  Goal0 = beyond(Num, Word).
+
+group([Word]) -->
+  { Sequence = [A]
+  ; Sequence = [0,A]
+  ; Sequence = [0,0,A]
+  },
+  Sequence,
+  { unit(A, Word) }.
+
+% double digit handling
+group([ten]) --> [1,0].
+group([ten]) --> [0,1,0].
+group([W0,W1]) -->
+  { Sequence = [A,B]
+  ; Sequence = [0,A,B]
+  },
+  Sequence,
+  { B #> 0,
+    A0 #= A*10,
+    ten(A0, W0),
+    unit(B, W1)
+  }.
+
+group([Word]) -->
+  { Sequence = [A,B]
+  ; Sequence = [0,A,B]
+  },
+  Sequence,
+  { A0 #= A*10,
+    N #= A0+B,
+    ten(N, Word)
+  }.
+
+% triple digit handling
+group(Word) -->
+  [A,B,C],
+  { A #> 0,
+    unit(A, W0),
+    phrase(group(W2), [B,C]),
+    format(atom(W1), "~a hundred", [W0]),
+    append([W1], W2, Word)
+  }.
+
+
+% single digit numbers
+unit(0, zero).
+unit(1, one).
+unit(2, two).
+unit(3, three).
+unit(4, four).
+unit(5, five).
+unit(6, six).
+unit(7, seven).
+unit(8, eight).
+unit(9, nine).
+
+% larger numbers
+ten(11, eleven).
+ten(12, twelve).
+ten(13, thirteen).
+ten(14, fourteen).
+ten(15, fifteen).
+ten(16, sixteen).
+ten(17, seventeen).
+ten(18, eighteen).
+ten(19, nineteen).
+ten(20, twenty).
+ten(30, thirty).
+ten(40, forty).
+ten(50, fifty).
+ten(60, sixty).
+ten(70, seventy).
+ten(80, eighty).
+ten(90, ninety).
+
+
+beyond(10^3, thousand).
+beyond(10^6, million).
+beyond(10^9, billion).
+beyond(10^12, trillion).
+beyond(10^15, quadrillion).
+beyond(10^18, quintillion).
+beyond(10^21, sextillion).
+beyond(10^24, septillion).
+beyond(10^27, octillion).
+beyond(10^30, nonillion).
+beyond(10^33, decillion).
+beyond(10^36, undecillion).
+beyond(10^39, duodecillion).
+beyond(10^42, tredecillion).
+beyond(10^45, quattuordecillion).
+beyond(10^48, quindecillion).
+beyond(10^51, sexdecillion).
+beyond(10^54, septendecillion).
+beyond(10^57, octodecillion).
+beyond(10^60, novemdecillion).
+beyond(10^63, vigintillion).
+
+
---
+++ number_to_word.pl
@@ -0,0 +1,89 @@
+
+:- module(number_to_word,
+     [ format_word/2 ]).
+
+:- use_module(groups).
+:- use_module(library(list_util)).
+:- use_module(library(clpfd)).
+
+
+% 0 is the same as [[]]
+
+
+format_word(Number, Word) :-
+  apply_suffix(Number, Digits), !,
+  (  Digits = [[]]
+  -> Word = "zero"
+  ;  format_word_(Digits, Words0),
+     reverse(Words0, Words1),
+     foldl(atomic_concat, Words1, '', Atom),
+     atom_string(Atom, Word)
+  ).
+
+format_word_([], []).
+format_word_([List|Rest], [Word|Words]) :-
+  (  List = L-S
+  -> atomic_list_concat(L, ' ', L0),
+     format(atom(Word), "~a ~a, ", [L0, S])
+  ;  atomic_list_concat(List, ' ', Word)
+  ),
+  format_word_(Rest, Words).
+
+
+apply_suffix(Number, Word) :-
+  number_digits(Number, Digits-Len),
+  apply_suffix_(Digits, Word0, Len),
+  exclude(=([]-_), Word0, Word).
+
+apply_suffix_([Last], [Word], _) :-
+  apply_phrase(Last, Word0),
+  exclude(=(zero), Word0, Word).
+
+apply_suffix_([G|Gs], [W-Suffix|Ws], N) :-
+  apply_phrase(G, W0),
+  exclude(=(zero), W0, W),
+  Index is 10^N,
+  beyond(Index, Suffix),
+  N0 is N-3,
+  apply_suffix_(Gs, Ws, N0).
+
+apply_phrase(List, Word) :-
+  phrase(group(Word), List).
+
+
+%% number_digits(+Num, -Digits)
+%
+%  Digits is a list of digits that represents an integer Num. Digits is divided
+%  into groups of 3. Digits will unify with a list of digits paired to its own
+%  length.
+
+number_digits(Num, Digits-Length) :-
+  number_chars(Num, Chars),
+  reverse(Chars, Rchars),
+  digits_triplets(Rchars, RDigits, Length),
+  reverse(RDigits, Digits).
+
+
+digits_triplets(Chars, Digits, Length) :-
+  digits_triplets_(Chars, Digits, -3, Length).
+
+digits_triplets_([], [], L, L) :- !.
+digits_triplets_([A], [[AN]], L0, L) :- !,
+  L is L0+3,
+  atom_number(A, AN).
+  
+digits_triplets_([A,B], [[BN,AN]], L0, L) :- !,
+  L is L0+3,
+  maplist(atom_number, [A,B], [AN,BN]).
+
+digits_triplets_([A,B,C|Rest], [[CN,BN,AN]|Triplets], L0, L) :-
+  L1 is L0+3,
+  maplist(atom_number, [A,B,C], [AN,BN,CN]),
+  digits_triplets_(Rest, Triplets, L1, L).
+
+
+
+
+
+
+
