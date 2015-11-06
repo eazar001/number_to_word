b1997da91f1c3102153b341955ba7dd86f77e805
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-06 00:10:00 -0800

    Restructing DCG to work in all directions and in partial mode.
    The original design was designed in a such a way that it only worked in
    the forward mode. Partial modes were also not suported. This is also
    much more efficient than the original design as well. The main drawback
    here is that representations are not directly in integer and string
    format. Structure digit and word lists are required for this design.
    Nonetheless, abstractions can be written atop this new structure to
    remedy said caveats.

--- prolog/groups.pl
+++ prolog/groups.pl
@@ -1,113 +1,131 @@
 
 :- module(groups,
-     [ group//1
-      ,unit/2
-      ,ten/2
-      ,beyond/2 ]).
+     [ gen//1 ]).
 
+:- initialization compile_predicates([gen//1]).
 
 :- use_module(library(clpfd)).
+:- use_module(library(dcg/basics)).
+
+:- dynamic gen//1.
+
+term_expansion(beyond(N, Word), Rule) :-
+  dynamic(gen//1),
+  succ(N0, N),
+  atom_concat(x_, N, Name),
+  atom_concat(x_, N0, Name0),
+  Head =.. [Name, [W1,Word|W2]],
+  Prev =.. [Name0, W2],
+  Rule = ( Head --> x(W1), call(Prev) ),
+  Gen =.. [Name, A, B, C],
+  GenRule = ( gen(A,B,C) :- Gen ),
+  assertz(GenRule).
 
 
-term_expansion(beyond(Goal, Word), Goal0) :-
-  Num is Goal,
-  Goal0 = beyond(Num, Word).
+
+gen(Ws) --> x(Ws).
+gen(Ws) --> x_2(Ws).
 
 
-group([Word]) -->
-  { Sequence = [A]
-  ; Sequence = [0,A]
-  ; Sequence = [0,0,A]
-  },
-  Sequence,
-  { unit(A, Word) }.
+x_2([W1,thousand|W2]) -->
+  x(W1),
+  x(W2).
 
-% Double digit handling
-group([ten]) --> [1,0].
-group([ten]) --> [0,1,0].
-group([W0,W1]) -->
-  { Sequence = [A,B]
-  ; Sequence = [0,A,B]
-  },
-  Sequence,
-  { B #> 0,
-    A0 #= A*10,
-    ten(A0, W0),
-    unit(B, W1)
+
+x([Word]) -->
+  dig(zero),
+  dig(zero),
+  dig(Word).
+
+x([Word]) -->
+  dig(zero),
+  special(Word).
+
+x(Word) -->
+  dig(zero),
+  ten(W1),
+  dig(W2),
+  {  W2 == zero
+  -> Word = [W1]
+  ;  Word = [W1,W2]
   }.
 
-group([Word]) -->
-  { Sequence = [A,B]
-  ; Sequence = [0,A,B]
-  },
-  Sequence,
-  { A0 #= A*10,
-    N #= A0+B,
-    ten(N, Word)
-  }.
+x(Word) -->
+  dig(W1),
+  { W1 \== zero },
+  % Second digit is either single digit or special number
+  (  (  dig(zero),
+        dig(W2)
+     ;  special(W2)
+     ),
+     {  W2 == zero
+     -> Word = [W1,hundred]
+     ;  Word = [W1,hundred,W2]
+     }
+  ;  ten(W2),
+     dig(W3),
+     { W2 \== zero,
+       (  W3 == zero
+       -> Word = [W1,hundred,W2]
+       ;  Word = [W1,hundred,W2,W3]
+       )
+     }
+  ).
 
-% Triple digit handling
-group(Word) -->
-  [A,B,C],
-  { A #> 0,
-    unit(A, W0),
-    phrase(group(W2), [B,C]),
-    format(atom(W1), "~a hundred", [W0]),
-    append([W1], W2, Word)
-  }.
+
+dig(zero) --> [0].
+dig(one) --> [1].
+dig(two) --> [2].
+dig(three) --> [3].
+dig(four) --> [4].
+dig(five) --> [5].
+dig(six) --> [6].
+dig(seven) --> [7].
+dig(eight) --> [8].
+dig(nine) --> [9].
 
 
-% Single digit numbers
-unit(0, zero).
-unit(1, one).
-unit(2, two).
-unit(3, three).
-unit(4, four).
-unit(5, five).
-unit(6, six).
-unit(7, seven).
-unit(8, eight).
-unit(9, nine).
+special(ten) --> [1,0].
+special(eleven) --> [1,1].
+special(twelve) --> [1,2].
+special(thirteen) --> [1,3].
+special(fourteen) --> [1,4].
+special(fifteen) --> [1,5].
+special(sixteen) --> [1,6].
+special(seventeen) --> [1,7].
+special(eighteen) --> [1,8].
+special(nineteen) --> [1,9].
 
-% Larger numbers
-ten(11, eleven).
-ten(12, twelve).
-ten(13, thirteen).
-ten(14, fourteen).
-ten(15, fifteen).
-ten(16, sixteen).
-ten(17, seventeen).
-ten(18, eighteen).
-ten(19, nineteen).
-ten(20, twenty).
-ten(30, thirty).
-ten(40, forty).
-ten(50, fifty).
-ten(60, sixty).
-ten(70, seventy).
-ten(80, eighty).
-ten(90, ninety).
 
-% Everything after
-beyond(10^3, thousand).
-beyond(10^6, million).
-beyond(10^9, billion).
-beyond(10^12, trillion).
-beyond(10^15, quadrillion).
-beyond(10^18, quintillion).
-beyond(10^21, sextillion).
-beyond(10^24, septillion).
-beyond(10^27, octillion).
-beyond(10^30, nonillion).
-beyond(10^33, decillion).
-beyond(10^36, undecillion).
-beyond(10^39, duodecillion).
-beyond(10^42, tredecillion).
-beyond(10^45, quattuordecillion).
-beyond(10^48, quindecillion).
-beyond(10^51, sexdecillion).
-beyond(10^54, septendecillion).
-beyond(10^57, octodecillion).
-beyond(10^60, novemdecillion).
-beyond(10^63, vigintillion).
+ten(twenty) --> [2].
+ten(thirty) --> [3].
+ten(forty) --> [4].
+ten(fifty) --> [5].
+ten(sixty) --> [6].
+ten(seventy) --> [7].
+ten(eighty) --> [8].
+ten(ninety) --> [9].
+
+
+beyond(3, million).
+beyond(4, billion).
+beyond(5, trillion).
+beyond(6, quadrillion).
+beyond(7, quintillion).
+beyond(8, sextillion).
+beyond(9, septillion).
+beyond(10, octillion).
+beyond(11, nonillion).
+beyond(12, decillion).
+beyond(13, undecillion).
+beyond(14, duodecillion).
+beyond(15, tredecillion).
+beyond(16, quattuordecillion).
+beyond(17, quindecillion).
+beyond(18, sexdecillion).
+beyond(19, septendecillion).
+beyond(20, octodecillion).
+beyond(21, novemdecillion).
+beyond(22, vigintillion).
+
 
