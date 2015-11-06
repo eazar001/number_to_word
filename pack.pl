effffce7d27c0ab4e3bd060609be26dc515a263a
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-06 11:59:38 -0800

    Oops, bump to 0.3.2.

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(number_to_word).
 title('Integer to English word conversions').
-version('0.3.0').
+version('0.3.2').
 author('Ebrahim Azarisooreh', 'ebrahim.azarisooreh@gmail.com').
 home('https://github.com/eazar001/number_to_word').
 keywords([numbers,words,integers]).
-download('https://github.com/eazar001/number_to_word/archive/v0.3.0.zip').
+download('https://github.com/eazar001/number_to_word/archive/v0.3.2.zip').
