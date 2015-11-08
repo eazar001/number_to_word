fc5b615ad2556aac407a7e1b5ea10a22be3d9f1b
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-07 18:46:32 -0800

    Updating pack.pl.

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(number_to_word).
 title('Integer to English word conversions').
-version('0.3.4').
+version('0.4.1').
 author('Ebrahim Azarisooreh', 'ebrahim.azarisooreh@gmail.com').
 home('https://github.com/eazar001/number_to_word').
 keywords([numbers,words,integers]).
-download('https://github.com/eazar001/number_to_word/archive/v0.3.4.zip').
+download('https://github.com/eazar001/number_to_word/archive/v0.4.1.zip').
